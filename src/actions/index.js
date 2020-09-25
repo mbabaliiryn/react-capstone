a21991b744ba45235db91fd36eb2410da07feafa
mbabali iryn <mbabaliiryn@gmail.com>
2020-09-25 17:23:47 +0300

    create filterRecipe actions as a function

---
+++ src/actions/index.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import axios from 'axios';
+import { FETCH_RECIPIES_REQUEST, FETCH_RECIPIES_SUCCESS, FETCH_RECIPIES_FAILURE } from './actionTypes';
+
+export const fetchRecipiesRequest = () => ({
+  type: FETCH_RECIPIES_REQUEST,
+});
+
+export const fetchRecipiesSuccess = recipies => ({
+  type: FETCH_RECIPIES_SUCCESS,
+  payload: recipes,
+});
+
+export const fetchRecipiesFailure = error => ({
+  type: FETCH_RECIPIES_FAILURE,
+  payload: error,
+});
+
+export const filterRecipe = userId => ({
+  type: FILTER_RECIPE,
+  payload: userId,
+});
+
+export const fetchRecipes = () => dispatch => {
+  dispatch(fetchRecipesRequest);
+  axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+    const recipies = response.data;
+    dispatch(fetchRecipiesSuccess(recipies));
+  }).catch(error => {
+    const errorMsg = error.message;
+    dispatch(fetchRecipiesFailure(errorMsg));
+  });
+};
