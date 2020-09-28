0718b88a663e4e3ffabda0d7f1d62178587d5ac1
mbabali iryn <mbabaliiryn@gmail.com>
2020-09-28 13:58:10 +0300

    Updates

---
+++ precache-manifest.9cf76aa431d41cd9279fd2f9639e4a50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecaac29b3ad46e0dd1e769480aa7c83f",
+    "url": "/react-capstone/index.html"
+  },
+  {
+    "revision": "8b3f32ac7de3b0b00f23",
+    "url": "/react-capstone/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "1ed6c52920c247ac8be8",
+    "url": "/react-capstone/static/css/main.c9af426b.chunk.css"
+  },
+  {
+    "revision": "8b3f32ac7de3b0b00f23",
+    "url": "/react-capstone/static/js/2.1e1c54ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-capstone/static/js/2.1e1c54ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed6c52920c247ac8be8",
+    "url": "/react-capstone/static/js/main.5d56ce17.chunk.js"
+  },
+  {
+    "revision": "19be803387e5090b207e",
+    "url": "/react-capstone/static/js/runtime-main.bb1c5314.js"
+  }
+]);
